ada9a4af6a004d74a1c83a5d6d0ff90e8e743c1e
Alexander Williams <Alexander.THG@knights.ucf.edu>
2021-09-01 16:42:17 -0400

    Zork 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
- 
+
 namespace Zork
 {
     class Program
@@ -8,8 +8,39 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
+            Commands command = Commands.UNKOWN;
+
+            while(command != Commands.QUIT)
+            {
+                Console.Write("> ");
+
+                command = ToCommand(Console.ReadLine().Trim().ToUpper());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door. A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}.";
+                        break;
+                    
+                    default:
+                        outputString = "Unknown command";
+                        break;                       
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKOWN;
     }
