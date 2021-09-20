bb74c8e9fcfb6cea6606f9319b6f3b77629100af
Alexander Williams <Alexander.THG@knights.ucf.edu>
2021-09-20 18:16:05 -0400

    Zork 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -5,7 +5,7 @@ namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -17,13 +17,15 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
+            InitializeRoomDescription();
+
             Commands command = Commands.UNKOWN;
 
             while (command != Commands.QUIT)
             {
-                Console.Write($"{CurrentRoom}\n> ");
+                Console.Write($"{CurrentRoom.Name}\n> ");
 
-                command = ToCommand(Console.ReadLine().Trim().ToUpper());
+                command = ToCommand(Console.ReadLine().Trim());
 
                 string outputString;
 
@@ -34,7 +36,7 @@ namespace Zork
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door. A rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        outputString = CurrentRoom.Description;
                         break;
 
                     case Commands.NORTH:
@@ -87,13 +89,26 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKOWN;
 
-        private static readonly string[,] Rooms =
+        private static readonly Room[,] Rooms =
             {
-                 {"Rocky Trail", "South of House", "Canyon View"},
-                 {"Forest", "West of House", "Behind House"},
-                 {"Dense Woods","North of House","Clearing"}
+                 {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View")},
+                 {new Room("Forest"), new Room("West of House"), new Room("Behind House")},
+                 {new Room("Dense Woods"), new Room("North of House"),new Room("Clearing")}
             };
 
         private static (int Row, int Column) Location = (1,1);
+
+        private static void InitializeRoomDescription()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here and all the windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall";
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        }
     }
 }
