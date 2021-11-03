cbc015e6ea59588adfe59f77e8595d68aed349fb
Alexander Williams <Alexander.THG@knights.ucf.edu>
2021-11-03 16:23:12 -0400

    Broken Commit 3

---
+++ Zork/Player.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Zork
+{
+    public class Player
+    {
+        public World World { get; }
+
+        [JsonIgnore]
+        public Room CurrentRoom { get; set; }
+
+        public string SetLocation
+        {
+            get
+            {
+                return CurrentRoom?.Name;
+            }
+            set
+            {
+                CurrentRoom = World?.RoomByName.GetValueOrDefault(value);
+            }
+        }
+
+        public Room PerviousRoom { get; set; }
+
+        public Player(World world, string startingLocation)
+        {
+            World = world;
+
+            SetLocation = startingLocation;
+        }
+
+        public bool Move(Directions directions)
+        {
+            bool IsValidMove = CurrentRoom.Neighbors.TryGetValue(directions, out Room neighbor);
+
+            if(IsValidMove)
+            {
+                CurrentRoom = neighbor;
+            }
+
+            return false;
+        }
+
+        public int Score = 0;
+
+        public int Movement = 0;
+    }
+}
---
+++ Zork/Room.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Zork
+{
+    public class Room
+    {
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        [JsonIgnore]
+        public Dictionary<Directions, Room> Neighbors {get; set;}
+
+        [JsonProperty (PropertyName = "Neighbors")]
+        public Dictionary<Directions, string> NeighborsNames { get; set; }
+
+        public Room(string name, string description = "")
+        {
+            Name = name;
+    
+            Description = description;
+        }
+
+        public override string ToString() => Name;
+
+        public void UpdateNeighbors(World world)
+        {
+            Neighbors = new Dictionary<Directions, Room>();
+
+            foreach (var (direction, name) in NeighborsNames)
+            {
+               Neighbors.Add(direction, world.RoomByName[name]);
+            }
+        }
+    }
+}
